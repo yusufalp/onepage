a5bcf3c0cef5406778070bd2c8de35785f0503d5
Yusuf Alp <alpyusuf007@gmail.com>
2022-04-24 08:39:11 -0700

    toggle hamburger menu

--- index.js
+++ index.js
@@ -80,5 +80,28 @@ for (let tech of techStackList) {
   about.appendChild(techImg);
 }
 
+const menu = document.querySelector(".nav-links");
+const menuItems = document.querySelectorAll(".nav-links-item");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menu-icon");
 
+function toggleMenu() {
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("show-menu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
 
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(
+  function (menuItem) {
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
